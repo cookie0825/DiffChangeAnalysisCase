cab0edad651cdf6a86cfa235fc711286ca6f5806
cookie0825 <1402049179@qq.com>
2024-05-24 14:48:09 +0800

    add a testMethod for parameter  null pointer check

--- src/main/java/org/example/Base.java
+++ src/main/java/org/example/Base.java
@@ -1,2 +1,17 @@
-package org.example;public class Base {
+package org.example;
+
+public class Base {
+    boolean flag;
+
+    public Base(boolean flag) {
+        this.flag = flag;
+    }
+
+    public boolean isFlag() {
+        return flag;
+    }
+
+    public void setFlag(boolean flag) {
+        this.flag = flag;
+    }
 }
