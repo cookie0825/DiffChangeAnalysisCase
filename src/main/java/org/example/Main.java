a426dd22f9fff5250c9ea8fa4b3228f40a20aa2b
cookie0825 <1402049179@qq.com>
2024-05-24 15:18:31 +0800

    add a Base variable base2 to check null use after var==null

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,8 +6,11 @@ public class Main {
         Base base = null;
     }
 
-    public static void testMethod(Base base){
-        System.out.println(base.isFlag());
+    public static void testMethod4NonEnsure(Base base, Base base2){
+        if (base2 == null){
+            System.out.println("base2 is null");
+        }
+        System.out.println(base2.isFlag());
         System.out.println("testMethod");
     }
 }
